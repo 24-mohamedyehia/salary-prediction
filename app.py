1eab02c176a01197d5fc39f35e852c7055b027d3
mohamed-y <24-mohamedyehia>
2025-03-02 04:28:48 +0200

    add app.py file

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import numpy as np
+from joblib import load
+
+# Load Model
+model = load('./employee_salary_model.joblib')
+
+st.title("Employee Salary Prediction App")
+st.write("Predict employee salary based on years of experience")
+
+# User Input
+experience = st.number_input("Enter Years of Experience:", min_value=0.0, max_value=50.0, step=0.1)
+
+if st.button("Predict"):
+    if experience == 0:
+        st.warning("Please enter a valid number of years")
+    else:
+        salary = model.predict(np.array([[experience]]))
+        st.success(f"The Predicted Salary is: {salary[0]:,.2f} EGP")
